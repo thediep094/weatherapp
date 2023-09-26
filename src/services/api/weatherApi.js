a9ea3cc9e612a39b1f5a1579c629a309061e1174
diepnguyenthe <diep.nguyenthe@vti.com.vn>
2023-09-26 15:09:27 +0700

    update

--- src/services/api/weatherApi.js
+++ src/services/api/weatherApi.js
@@ -2,6 +2,20 @@ import axios from "axios";
 import { API_KEY, API_PREFIX } from "../../common/index";
 export const weatherApi = {
   fetchWeather(params) {
+    const url = `${API_PREFIX}/data/2.5/weather`;
+    return axios.get(url, {
+      params: {
+        ...params,
+        appid: API_KEY,
+        lang: "vi",
+        units: "metric",
+      },
+    });
+  },
+};
+
+export const forecastApi = {
+  fetchForecast(params) {
     const url = `${API_PREFIX}/data/2.5/forecast`;
     return axios.get(url, {
       params: {
